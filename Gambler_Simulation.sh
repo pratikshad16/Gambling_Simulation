5a04a55547e7e41c42d94f0dd4b95b4eab42a4aa
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-19 22:49:36 +0530

    Added: functionality for resigning for the day is added

--- Gambler_Simulation.sh
+++ Gambler_Simulation.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -x
 STAKE=100
 BET=1
-result=$(( RANDOM%2 ))
-if [ $result -eq 1 ]
-then
-	STAKE=$((STAKE+BET))
-else
-	STAKE=$((STAKE-BET))
-fi 
+high=$(( $STAKE + $(( ($STAKE*50)/100)) ));
+low=$((  $STAKE - $(( ($STAKE*50)/100)) ));
+while [ $STAKE -gt $low ] && [ $STAKE -lt $high ]
+do
+	result=$((RANDOM%2))
+
+	if [ $result -eq 1 ]
+	then
+		STAKE=$((STAKE + BET))
+	else
+		STAKE=$((STAKE - BET))
+	fi
+done
+echo "you have done with  today's bets" 

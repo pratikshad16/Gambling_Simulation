85db13b3c71b6826dff249fccaf0f6fb2eece25d
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-20 00:49:40 +0530

    Added: functionality for caculating totak amt for 20 days is added

--- Gambler_Simulation.sh
+++ Gambler_Simulation.sh
@@ -1,17 +1,50 @@
-#!/bin/bash -x
+##!/bin/bash -x
 STAKE=100
 BET=1
-high=$(( $STAKE + $(( ($STAKE*50)/100)) ));
-low=$((  $STAKE - $(( ($STAKE*50)/100)) ));
-while [ $STAKE -gt $low ] && [ $STAKE -lt $high ]
-do
-	result=$((RANDOM%2))
+MAX_DAYS=20
+noOfWins=0
+noOfLoss=0
+winning_days=0
+loosing_days=0
+STAKE_PERCENTAGE=$((((STAKE*50))/100))
+dailyStake=$STAKE
+betCalculation(){
+	STAKE=100
+	dailyStake=$STAKE
+	STAKE_PERCENTAGE=$((((STAKE*50))/100))
+	while [ $STAKE -gt $((dailyStake-STAKE_PERCENTAGE)) ] && [ $STAKE -lt $((dailyStake+STAKE_PERCENTAGE)) ]
+	do
+		result=$((RANDOM%2))
 
-	if [ $result -eq 1 ]
+		if [ $result -eq 1 ]
+		then
+			STAKE=$((STAKE + BET))
+			(( noOfWins++ ))
+		else
+			STAKE=$((STAKE - BET))
+			(( noOfLoss++ ))
+		fi
+	done
+	if [ $noOfWins -gt $noOfLoss ]
 	then
-		STAKE=$((STAKE + BET))
+		((winning_days++))
 	else
-		STAKE=$((STAKE - BET))
+		((loosing_days++))
 	fi
+}
+amountCalculation(){
+	if [ $winning_days -gt $loosing_days ]
+	then
+		winning_amt=$((winning_days*STAKE_PERCENTAGE))
+		echo "winning amount is: $winning_amt"
+	else
+		loosing_amt=$((loosing_days*STAKE_PERCENTAGE))
+		echo "loosing amount is: $loosing_amt"
+	fi
+}
+for (( i=0;  i<MAX_DAYS; i++ ))
+do
+betCalculation
 done
-echo "you have done with  today's bets" 
+amountCalculation
+
